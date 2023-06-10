c58ee9310c6682c19f95f3222e2a60762f11dc42
Joshibibek5 <joshibibek5@gmail.com>
2023-06-10 12:59:31 +0545

    done

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import { robots } from './robots';
-import Cardarray from './Cardarray';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
